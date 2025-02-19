9c7cd06e8061ec0a8139c4886c0632c94ce1e591
venkatreddy2001 <venkatreddymeka02@gmail.com>
2025-02-19 04:27:42 +0000

    Lab submission 5

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const Products = require('./products')
 const autoCatch = require('./lib/auto-catch')
-
+const Orders = require('./orders')
 /**
  * Handle the root route
  * @param {object} req
@@ -50,9 +50,11 @@ async function getProduct(req, res, next) {
  * @param {object} res 
  */
 async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+  const product = await Products.create(req.body)
+  res.json(product)
 }
+
+
 
 /**
  * Edit a product
@@ -61,8 +63,15 @@ async function createProduct(req, res) {
  * @param {function} next
  */
 async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
+}
+
+async function editOrder(req, res, next) {
+  const change = req.body
+  const order = await Orders.edit(req.params.id, change)
+  res.json(order)
 }
 
 /**
@@ -72,7 +81,32 @@ async function editProduct(req, res, next) {
  * @param {*} next 
  */
 async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+
+async function deleteOrder(req, res, next) {
+  const response = await Orders.destroy(req.params.id)
+  res.json(response)
+}
+
+
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
 }
 
 module.exports = autoCatch({
@@ -81,5 +115,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder,
+  editOrder,
+  deleteOrder
 });
